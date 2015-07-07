513ad2debf96c83fb5e746072de58ca516b68d0f
amg26 <amg26@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2015-07-07 15:46:47 +0000

    Initial commit
    git-svn-id: https://plugins.svn.wordpress.org/generate-image-alt-tags/trunk@1194208 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ generate-image-alt-tags.php
@@ -0,0 +1,27 @@
+<?php
+/*
+* Plugin Name: Generate Image Alt Tags
+* Plugin URI: http://andrewmgunn.com/generate-image-alt-tags/
+* Description: Automatically generate and update image Alt tags for images in the database where the Alt attribute is blank and make your site more SEO friendly.
+* Version: 1.0
+* Author: Andrew M. Gunn
+* Author URI: http://andrewmgunn.com
+* Text Domain: generate-image-alt-tags
+* License: GPL2
+*/
+
+defined( 'ABSPATH' ) or die( 'Plugin file cannot be accessed directly.' );
+//define( 'PLUGIN_DIR', dirname(__FILE__).'/' );
+//require_once('inc/options.php');
+add_action( 'admin_init', 'register_gen_alt_tag' );
+
+function register_gen_alt_tag() {
+	add_action( 'wp_enqueue_scripts', 'register_gen_alt_tag_includes' );
+}
+
+function register_gen_alt_tag_includes() {
+	wp_register_script( 'gen-alt-tag-scripts', plugins_url('inc/plugin-scripts.js', __FILE__), array('jquery'));
+  wp_register_style( 'gen-alt-tag-styles', plugins_url('inc/plugin-styles.css', __FILE__));
+  wp_enqueue_script( 'gen-alt-tag-scripts' );
+  wp_enqueue_style( 'gen-alt-tag-styles' );
+}
---
+++ readme.txt
@@ -0,0 +1,19 @@
+=== Generate Image Alt Tags ===
+Contributors: amg26
+Tags: image alt, alt tags, generate alt, seo, seo image
+Donate link: http://andrewmgunn.com/donate/
+Requires at least: 3.5
+Tested up to: 4.2.2
+Stable tag: 1.0
+License: GPLv2 or later
+License URI: http://www.gnu.org/licenses/gpl-2.0.html
+
+Automatically generate and update image Alt tags for images in the database where the Alt attribute is blank and make your site more SEO friendly.
+
+
+== Installation ==
+Extract the zip file and just drop the contents in the wp-content/plugins/ directory of your WordPress installation and then activate the Plugin from Plugins page.
+
+
+== Upgrade Notice ==
+Upgrade this plugin for more features!
