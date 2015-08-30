7b962834853f9b65b72ff7fa8e29bde65e9c1c35
amg26 <amg26@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2015-08-30 09:31:11 +0000

    Optimized scripts and added auto alt text saving on image upload.
    git-svn-id: https://plugins.svn.wordpress.org/generate-image-alt-tags/trunk@1234010 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- generate-image-alt-tags.php
+++ generate-image-alt-tags.php
@@ -1,9 +1,9 @@
 <?php
 /*
-* Plugin Name: Generate Image Alt Tags
-* Plugin URI: http://andrewmgunn.com/generate-image-alt-tags/
-* Description: Automatically generate and update image Alt tags for images in the database where the Alt attribute is blank and make your site more SEO friendly.
-* Version: 1.2
+* Plugin Name: Easy Image SEO Tags
+* Plugin URI: http://andrewmgunn.com/
+* Description: Automatically generate and save alt tags to database when images are uploaded & clientside SEO image tag optimization.
+* Version: 1.2.1
 * Author: Andrew M. Gunn
 * Author URI: http://andrewmgunn.com
 * Text Domain: generate-image-alt-tags
@@ -13,15 +13,68 @@
 defined( 'ABSPATH' ) or die( 'Plugin file cannot be accessed directly.' );
 //define( 'PLUGIN_DIR', dirname(__FILE__).'/' );
 //require_once('inc/options.php');
-add_action( 'admin_init', 'register_gen_alt_tag' );
+add_action( 'admin_init', 'register_siat_scripts_easy' );
 
-function register_gen_alt_tag() {
-	add_action( 'wp_enqueue_scripts', 'register_gen_alt_tag_includes' );
+function register_siat_scripts_easy() {
+	add_action( 'wp_enqueue_scripts', 'register_siat_includes_easy' );
 }
 
-function register_gen_alt_tag_includes() {
-	wp_register_script( 'gen-alt-tag-scripts', plugins_url('inc/plugin-scripts.js', __FILE__), array('jquery'));
-  wp_register_style( 'gen-alt-tag-styles', plugins_url('inc/plugin-styles.css', __FILE__));
-  wp_enqueue_script( 'gen-alt-tag-scripts' );
-  wp_enqueue_style( 'gen-alt-tag-styles' );
+function register_siat_includes_easy() {
+	wp_register_script( 'siat_scripts_easy', plugins_url('inc/siat-scripts.js', __FILE__), array('jquery'));
+  wp_register_style( 'siat_styles_easy', plugins_url('inc/siat-styles.css', __FILE__));
+  wp_enqueue_style( 'siat_scripts_easy' );
+	wp_enqueue_script( 'siat_styles_easy' );
+
+}
+
+add_filter('add_attachment', 'insert_image_alt_tag_easy', 10, 2);
+
+function insert_image_alt_tag_easy($post_ID) {
+	$title = get_the_title($post_ID);
+
+	if ( ! add_post_meta( $post_ID, '_wp_attachment_image_alt', $title, true ) ) {
+	   update_post_meta ( $post_ID, '_wp_attachment_image_alt', $title );
+	}
+}
+
+function batch_update_image_tags_easy() {
+	$args = array(
+    'post_type' => 'attachment',
+    'numberposts' => -1,
+    'post_status' => null,
+    'post_parent' => null, // any parent
+    );
+
+	$attachments = get_posts($args);
+	if ($attachments) {
+	    foreach ($attachments as $post) {
+	        setup_postdata($post);
+	        the_title();
+	        the_attachment_link($post->ID, false);
+	        the_excerpt();
+	    }
+	}
+}
+
+add_action( 'admin_menu', 'seo_image_tags_add_admin_menu_easy' );
+
+
+function seo_image_tags_add_admin_menu_easy(  ) {
+
+	add_submenu_page( 'tools.php', 'Easy Image SEO Tags', 'Image SEO Tags', 'manage_options', 'easy_image_seo_tags', 'seo_image_tags_options_page_easy' );
+
+}
+
+
+function seo_image_tags_options_page_easy(  ) {
+
+	?>
+	<form action='' method='post'>
+
+		<h2><b>SEO Image Tags</b></h2>
+		THIS IS STILL UNDER DEVELOPMENT.
+		<input type="submit" name="tag-update" id="tag-updater" value="Generate tags and Update Database" onclick=""  />
+	</form>
+	<?php
+
 }
--- readme.txt
+++ readme.txt
@@ -1,15 +1,16 @@
 === Generate Image Alt Tags ===
 Contributors: amg26
-Tags: image alt, alt tags, generate alt, seo, seo image
+Tags: seo image, image tags, alt tags, seo image tags
 Donate link: http://andrewmgunn.com/donate/
 Requires at least: 3.5
-Tested up to: 4.2.2
-Stable tag: 1.2
+Tested up to: 4.3
+Stable tag: 1.2.1
 License: GPLv2 or later
 License URI: http://www.gnu.org/licenses/gpl-2.0.html
 
-Automatically generate and update image Alt tags for images in the database where the Alt attribute is blank and make your site more SEO friendly.
+SEO Image Tags puts an end to ever have to worry about getting HTML validation errors and warnings due to lack of alt tags on images and media. Alt tags are dynamically generated and saved to the database automatically any time an image is uploaded, no configuration or headache required!
 
+And for the situations where images are statically hardcoded throughout and don\'t pull alt tags into them, we got you covered. There is clientside scripting to make sure every image has all the needed attributes for SEO friendly images and websites.
 
 == Installation ==
 Extract the zip file and just drop the contents in the wp-content/plugins/ directory of your WordPress installation and then activate the Plugin from Plugins page.
